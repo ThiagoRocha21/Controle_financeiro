6911f8ae2b96af2c54216660e8ea889130130122
Thiago de B. C. Rocha <129116830+ThiagoRocha21@users.noreply.github.com>
2025-11-19 16:03:51 -0300

    Add files via upload

---
+++ ControleFinancas/Controle-financas-Java/src/App.java
@@ -0,0 +1,10 @@
+import DB.SQLiteConnection;
+
+import java.sql.Connection;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.print(SQLiteConnection.Connection());
+
+    }
+}
