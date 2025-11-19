a7f4ab29756838ee02a64799e6e6be8e50f008b4
Thiago de B. C. Rocha <129116830+ThiagoRocha21@users.noreply.github.com>
2025-11-19 17:54:57 -0300

    Add 'produto_final' table creation in SQLiteConnection
    Added creation of 'produto_final' table to SQLite connection.

--- Controle_financeiro-main/ControleFinancas/Controle-financas-Java/src/DB/SQLiteConnection.java
+++ Controle_financeiro-main/ControleFinancas/Controle-financas-Java/src/DB/SQLiteConnection.java
@@ -12,24 +12,36 @@ public class SQLiteConnection {
 
         String createTableProduct = "CREATE TABLE IF NOT EXISTS produtos ("+
                                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                                "name TEXT NOT NULL,"+
+                                "name VARCHAR (30),"+
                                 "quantidade INTEGER"+
                                 ")";
 
         String createTableFinancial = "CREATE TABLE IF NOT EXISTS financeiro ("+
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                "preco_unitario FLOAT,"+
-                "preco_total FLOAT,"+
-                "produto_id INTEGER,"+
-                "FOREIGN KEY (produto_id) REFERENCES produtos(id)"+
-                ")";
+                                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                                    "preco_unitario FLOAT,"+
+                                    "preco_total FLOAT,"+
+                                    "produto_id INTEGER,"+
+                                    "FOREIGN KEY (produto_id) REFERENCES produtos (id)"+
+                                    ")";
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-            Statement stmt = (conn.createStatement())) {
+
+        String createTableFinalProduct = "CREATE TABLE IF NOT EXISTS produto_final ("+
+                                         "produto_final_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                                         "nome_produto_final VARCHAR (20),"+
+                                         "quantidade INTEGER,"+
+                                         "preco FLOAT"+
+                                         ")";
+
+
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(DB_URL); Statement stmt = (conn.createStatement());
             //CRIA A TABELA PRODUCT
             stmt.execute(createTableProduct);
             //CRIA A TABELA FINANCIAL
             stmt.execute(createTableFinancial);
+            //CRIA A TABELA FinalProduct
+            stmt.execute(createTableFinalProduct);
             return "Tabela criada com sucesso✅ ou Conexão estabelecida com sucesso ✅";
 
         } catch (Exception e) {
